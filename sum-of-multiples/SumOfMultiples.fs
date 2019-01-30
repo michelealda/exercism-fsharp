12b7aaf0214570b6f92285099b2104dcbbb4633d
michelealda <michele.alda@gmail.com>
2019-01-30 11:19:57 +0100

    Solution to sum of multiples

---
+++ sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,11 @@
+﻿module SumOfMultiples
+
+let isMultiple (numbers: int list, v: int): bool =
+    numbers
+    |> List.filter (fun x -> x <> 0)
+    |> List.fold (fun acc n -> (v%n = 0) || acc) false
+
+let sum (numbers: int list) (upperBound: int): int =
+    [0 ..upperBound-1]
+    |> List.filter (fun x -> isMultiple(numbers, x))
+    |> List.sum
