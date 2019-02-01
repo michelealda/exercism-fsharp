13665f5a879ac9f8abd3d1fcf47436d62f8a8f4f
michelealda <michele.alda@gmail.com>
2019-02-01 09:28:39 +0100

    Solution to amstrong numbers

---
+++ armstrong-numbers/ArmstrongNumbers.fs
@@ -0,0 +1,12 @@
+﻿module ArmstrongNumbers
+
+let isArmstrongNumber (number: int): bool = 
+    let xs = number 
+            |> string
+            |> (fun s -> s.ToCharArray())
+            // convert from the ascii code to the int value
+            |> Seq.map (fun c -> int c - int '0') 
+                        
+    xs        
+    |> Seq.sumBy (fun x -> Seq.length xs |> pown x )
+    |> (=) number
