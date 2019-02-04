a975873bf3610ba0b5f773f74f2e37cfa5b67e6e
michelealda <michele.alda@gmail.com>
2019-02-04 11:57:26 +0100

    Solution to robot simulator

--- robot-simulator/RobotSimulator.fs
+++ robot-simulator/RobotSimulator.fs
@@ -11,7 +11,7 @@ type Robot = { direction: Direction; position: Position }
 
 let create direction position = {direction = direction; position = position}
 
-let rotate robot =
+let rotateRight robot =
     let newDirection = 
         match robot.direction with
         | North -> East
@@ -20,11 +20,20 @@ let rotate robot =
         | West -> North
     {robot with direction=newDirection }
 
+let rotateLeft robot =
+    let newDirection = 
+        match robot.direction with
+        | North -> West
+        | West -> South
+        | South -> East
+        | East -> North        
+    {robot with direction=newDirection }
+
 let advance robot =
     let newPosition = 
         match robot.direction with
-        | North -> (fun (x, y) -> (x+1, y))
-        | East -> (fun (x,y) -> (x, y+1))
+        | North -> (fun (x, y) -> (x, y+1))
+        | East -> (fun (x,y) -> (x+1, y))
         | South -> (fun (x,y) -> (x, y-1))
         | West -> (fun (x,y) -> (x-1, y))
     {robot with position = newPosition robot.position}
@@ -33,5 +42,6 @@ let move instructions robot =
     instructions
     |> Seq.fold (fun r c -> match c with 
                             | 'A' -> advance r
-                            | 'R' -> rotate r
+                            | 'R' -> rotateRight r
+                            | 'L' -> rotateLeft r
                             | _ -> r) robot
