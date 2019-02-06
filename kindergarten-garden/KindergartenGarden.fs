9d14f7e62e7a26a1c9d4b2070ea72b8d8672ed2e
michelealda <michele.alda@gmail.com>
2019-02-06 15:17:01 +0100

    Solution for kindergarden problem

---
+++ kindergarten-garden/KindergartenGarden.fs
@@ -0,0 +1,27 @@
+﻿module KindergartenGarden
+
+type Plant = 
+      Violets = 'V' 
+    | Radishes ='R'
+    | Clover = 'C'
+    | Grass = 'G'
+
+let Students = ["Alice"; "Bob"; "Charlie"; 
+ "David"; "Eve"; "Fred"; "Ginny";
+ "Harriet"; "Ileana"; "Joseph"; 
+ "Kincaid"; "Larry" ]
+
+let sliceGardens (index :int) (gardens: string[])  =
+    gardens
+    |> Seq.fold (fun acc g -> acc + (g.[(index*2)..(index*2)+1])) ""
+    
+let plants (diagram: string) (student:string) = 
+    match Seq.tryFindIndex ((=) student) Students with
+    | Some x -> diagram.Split [|'\n'|]
+                |> sliceGardens x
+                |> Seq.map (LanguagePrimitives.EnumOfValue)
+                |> List.ofSeq
+    | None -> List.empty<Plant>
+    
+
+    
