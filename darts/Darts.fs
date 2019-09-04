654b55913926764489c7c0109a2d7b12f5b1347b
michelealda <michele.alda@gmail.com>
2019-09-04 10:12:33 +0200

    darts solved

---
+++ darts/Darts.fs
@@ -0,0 +1,11 @@
+﻿module Darts
+open System
+let score (x: double) (y: double): int = 
+    (x * x) + (y * y) 
+    |> float 
+    |> Math.Sqrt
+    |> fun r ->
+        if r <= 1.0 then 10
+        else if r <= 5.0 then 5
+        else if r <= 10.0 then 1
+        else 0
