8ae9429ec7df2b2862a360170d2d5dc1a9276507
michelealda <michele.alda@gmail.com>
2019-09-04 10:44:13 +0200

    high scores solved

---
+++ high-scores/HighScores.fs
@@ -0,0 +1,20 @@
+﻿module HighScores
+
+let scores (values: int list): int list = values
+    
+let latest (values: int list): int = 
+    List.last values
+
+let personalBest (values: int list): int = 
+    List.max values
+
+let personalTopThree (values: int list): int list = 
+    values
+    |> List.sortDescending
+    |> fun l ->
+        match l with
+        | [] -> []
+        | [_] -> l
+        | [_; __] -> l
+        | _ -> l |> List.take 3
+
