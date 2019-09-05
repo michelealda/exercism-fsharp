6ab9580fb587132e6e779f2e4b72717a73982002
michelealda <michele.alda@gmail.com>
2019-09-05 13:19:02 +0200

    Anagram solved

---
+++ anagram/Anagram.fs
@@ -0,0 +1,16 @@
+﻿module Anagram
+open System
+
+let toKey x = 
+    x 
+    |> Seq.map Char.ToLower 
+    |> Seq.sort
+    |> Seq.toArray 
+    |> String
+
+let findAnagrams (sources: string list) (target: string) = 
+    let wordKey = toKey target
+    sources
+    |> List.filter (toKey >> (=) wordKey)
+    |> List.filter (fun s -> s.ToLower() <> target.ToLower())
+    
