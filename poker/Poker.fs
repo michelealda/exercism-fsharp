5710ac0b89c063faa6d89c245c23609af20e9f56
michelealda <michele.alda@gmail.com>
2019-11-04 12:56:04 +0100

    Started poker solution

---
+++ poker/Poker.fs
@@ -0,0 +1,24 @@
+﻿module Poker
+
+open System
+// TODO: implement this module
+type Suit =
+    | Heart
+    | Spade
+    | Club
+    | Diamond
+
+type Value =
+    | Number of int
+    | Jack
+    | Queen
+    | King
+    | Ace
+
+type Card =
+    { value: Value
+      suit: Suit }
+
+type Hand = Card list
+
+let bestHands xs = xs
