15638ff57cac4d8f8b3138e7a87d9f1adc246a4a
michelealda <michele.alda@gmail.com>
2019-11-05 10:07:45 +0100

    Solution to phone number

--- phone-number/PhoneNumber.fs
+++ phone-number/PhoneNumber.fs
@@ -1,13 +1,63 @@
 ﻿module PhoneNumber
+
 open System
-let removePunctuation input =
-    input
-    |> Seq.filter Char.IsDigit
-    
+
+let checkLetters xs =
+    match Seq.tryFind Char.IsLetter xs with
+    | Some _ -> Error "letters not permitted"
+    | None -> Ok xs
+
+let checkPunctuations xs =
+    match Seq.tryFind Char.IsPunctuation xs with
+    | Some _ -> Error "punctuations not permitted"
+    | None -> Ok xs
 
 
+let removeValidCharacters xs =
+    Seq.fold (fun acc c ->
+        if Seq.contains c [ '('; ')'; '.'; '+'; ' '; '-' ] then acc
+        else (Seq.append acc [ c ])) Seq.empty xs
+    |> Ok
 
-   
 
-    
-    
+let validateLenght xs =
+    match (Seq.length xs, Seq.head xs) with
+    | (10, _) -> Ok xs
+    | (11, '1') ->
+        xs
+        |> Seq.tail
+        |> Ok
+    | (11, _) -> Error "11 digits must start with 1"
+    | (x, _) when x < 10 -> Error "incorrect number of digits"
+    | _ -> Error "more than 11 digits"
+
+let validateAreaCode xs =
+    match Seq.head xs with
+    | '0' -> Error "area code cannot start with zero"
+    | '1' -> Error "area code cannot start with one"
+    | _ -> Ok xs
+
+let validateExchangeCode xs =
+    match Seq.item 3 xs with
+    | '0' -> Error "exchange code cannot start with zero"
+    | '1' -> Error "exchange code cannot start with one"
+    | _ -> Ok xs
+
+
+let mapToResult xs =
+    xs
+    |> Array.ofSeq
+    |> String
+    |> uint64
+    |> Ok
+
+let clean (s: string): Result<uint64, string> =
+    s
+    |> removeValidCharacters
+    |> Result.bind checkLetters
+    |> Result.bind checkPunctuations
+    |> Result.bind validateLenght
+    |> Result.bind validateAreaCode
+    |> Result.bind validateExchangeCode
+    |> Result.bind mapToResult
+ 
