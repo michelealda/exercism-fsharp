c211878977c9f156feb4573c95fa02f6e99c1a7b
michelealda <michele.alda@gmail.com>
2019-11-05 11:46:42 +0100

    Solution to PigLatin

---
+++ pig-latin/PigLatin.fs
@@ -0,0 +1,23 @@
+﻿module PigLatin
+
+let isVowel c = List.contains c [ 'a'; 'e'; 'i'; 'o'; 'u' ]
+
+let rec rule input =
+    match input with
+    | c :: _ when isVowel c -> input @ [ 'a'; 'y' ]
+    | 'x' :: 'r' :: _ -> input @ [ 'a'; 'y' ]
+    | 'y' :: 't' :: _ -> input @ [ 'a'; 'y' ]
+    | [ 'y'; c ] -> input @ [ 'a'; 'y' ]
+    | 'q' :: 'u' :: rest -> rest @ [ 'q'; 'u'; 'a'; 'y' ]
+    | c :: rest -> rule (rest @ [ c ])
+    | _ -> input
+
+let translateWord input =
+    input
+    |> rule
+    |> List.fold (fun acc c -> acc + (string c)) ""
+
+let translate (input: string) =
+    input.Split(' ')
+    |> Array.map (Seq.toList >> translateWord)
+    |> String.concat " "
