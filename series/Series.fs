d9d7e76dd56247612692230e4d4b916dfedea7c8
michelealda <michele.alda@gmail.com>
2019-11-06 09:03:13 +0100

    Solution to series

---
+++ series/Series.fs
@@ -0,0 +1,13 @@
+﻿module Series
+
+open System
+
+let slices (str: string) length =
+    match length with
+    | x when x <= 0 || x > Seq.length str -> None
+    | x ->
+        str
+        |> Seq.windowed x
+        |> Seq.map (String.Concat)
+        |> Seq.toList
+        |> Some
