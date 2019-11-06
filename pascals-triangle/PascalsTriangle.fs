5e0c71f725b60f608433d4c832b18e019fa4ca54
michelealda <michele.alda@gmail.com>
2019-11-06 14:56:19 +0100

    Solution to Pascal triangle

---
+++ pascals-triangle/PascalsTriangle.fs
@@ -0,0 +1,21 @@
+﻿module PascalsTriangle
+
+let generateNext xs =
+    xs
+    |> List.mapi (fun i k ->
+        match i with
+        | 0 -> 1
+        | j -> xs.[j - 1] + k)
+    |> fun ys -> ys @ [ 1 ]
+
+let rows numberOfRows: int list list =
+    match numberOfRows with
+    | 0 -> []
+    | _ ->
+        List.fold (fun acc _ ->
+            let next =
+                acc
+                |> List.last
+                |> generateNext
+            acc @ [ next ]) [ [ 1 ] ] [ 2 .. numberOfRows ]
+ 
