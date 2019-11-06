585883f8e429bd6e20d3c77221b7bc743e43c0de
michelealda <michele.alda@gmail.com>
2019-11-06 12:25:22 +0100

    Solution to House

---
+++ house/House.fs
@@ -0,0 +1,19 @@
+﻿module House
+
+let verses =
+    [ ("the horse and the hound and the horn", "that belonged to")
+      ("the farmer sowing his corn", "that kept")
+      ("the rooster that crowed in the morn", "that woke")
+      ("the priest all shaven and shorn", "that married")
+      ("the man all tattered and torn", "that kissed")
+      ("the maiden all forlorn", "that milked")
+      ("the cow with the crumpled horn", "that tossed")
+      ("the dog", "that worried")
+      ("the cat", "that killed")
+      ("the rat", "that ate")
+      ("the malt", "that lay in")
+      ("the house", "that Jack built.") ]
+
+let buildRhyme i = verses.[(12 - i)..11] |> List.fold (fun acc (sub, link) -> acc + " " + sub + " " + link) "This is"
+
+let recite startVerse endVerse: string list = [ startVerse .. endVerse ] |> List.map buildRhyme
