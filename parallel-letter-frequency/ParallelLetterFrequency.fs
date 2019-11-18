3ab7bb14f97e90bf87479ea3512cc60cd900dc81
michelealda <michele.alda@gmail.com>
2019-11-18 13:54:51 +0100

    Solution to parallel letter frequency

---
+++ parallel-letter-frequency/ParallelLetterFrequency.fs
@@ -0,0 +1,23 @@
+﻿module ParallelLetterFrequency
+
+open System
+
+let calculateFrequencyAsync (xs: string) =
+    async {
+        return xs.ToLowerInvariant()
+               |> Seq.filter (Char.IsLetter)
+               |> Seq.groupBy id
+               |> Seq.map (fun (k, s) -> (k, Seq.length s))
+    }
+
+let frequency (texts: string list) =
+    texts
+    |> List.map calculateFrequencyAsync
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> Seq.concat
+    |> Seq.fold (fun acc (c, x) ->
+        match Map.tryFind c acc with
+        | Some k -> acc.Add(c, k + x)
+        | None -> acc.Add(c, x)) Map.empty
+ 
