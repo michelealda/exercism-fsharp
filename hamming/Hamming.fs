4bbbc052f09b051afe718feade34ec3340d2b1ee
michelealda <michele.alda@gmail.com>
2020-02-27 10:03:38 +0100

    Solution to hamming

---
+++ hamming/Hamming.fs
@@ -0,0 +1,11 @@
+﻿module Hamming
+
+let distance (strand1: string) (strand2: string): int option =
+    match strand1.Length - strand2.Length with
+    | 0 ->
+        strand2
+        |> Seq.zip strand1
+        |> Seq.sumBy (fun (a, b) ->
+            if a = b then 0 else 1)
+        |> Some
+    | _ -> None
