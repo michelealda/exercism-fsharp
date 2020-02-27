5c42eb7b893086841fbcfa9332047a88f33bf147
michelealda <michele.alda@gmail.com>
2020-02-27 11:09:09 +0100

    Solution to allergies

---
+++ allergies/Allergies.fs
@@ -0,0 +1,23 @@
+﻿module Allergies
+
+open System
+
+type Allergen =
+    | Eggs = 1
+    | Peanuts = 2
+    | Shellfish = 4
+    | Strawberries = 8
+    | Tomatoes = 16
+    | Chocolate = 32
+    | Pollen = 64
+    | Cats = 128
+
+
+let allergicTo (codedAllergies: int) (allergen: Allergen) =
+    let intAllergen = (allergen |> int)
+    (codedAllergies &&& intAllergen) = intAllergen
+
+let list codedAllergies =
+    Enum.GetValues(typeof<Allergen>) :?> Allergen []
+    |> Array.filter (fun a -> allergicTo codedAllergies a)
+    |> List.ofArray
