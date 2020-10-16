8ff9d635c6867574f5313af7cdb9fe7470dfe884
Michele Alda <m.alda@vanlanschotkempen.com>
2020-10-16 16:31:42 +0200

    Dot-dsl solution

---
+++ dot-dsl/DotDsl.fs
@@ -0,0 +1,35 @@
+﻿module DotDsl
+
+type Element =
+    | Attr of (string * string)
+    | Node of string * (string * string) list
+    | Edge of string * string * (string * string) list
+
+let graph = List.sort
+
+let attr key value = Attr(key, value)
+
+let node key attrs = Node(key, attrs)
+
+let edge left right attrs = Edge(left, right, attrs)
+
+let attrs graph =
+    graph
+    |> List.choose (fun e ->
+        match e with
+        | Attr _ -> Some e
+        | _ -> None)
+
+let nodes graph =
+    graph
+    |> List.choose (fun e ->
+        match e with
+        | Node _ -> Some e
+        | _ -> None)
+
+let edges graph =
+    graph
+    |> List.choose (fun e ->
+        match e with
+        | Edge _ -> Some e
+        | _ -> None)
